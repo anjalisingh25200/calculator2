5b3a000d15d7c4072620bd041c061dd57f869af4
anjalisingh200025@gmail.com <anjalisingh200025@gmail.com>
2022-02-21 15:35:15 +0530

    first calculator

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express();
+app.use(bodyparser.urlencoded({ extended: true }))
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function (req, res) {
+
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var operator = req.body.option;
+
+    function calc(num1, num2, option) {
+        var output = 0;
+
+        if (operator == "+") {
+            output = num1 + num2;
+            res.send("The result " + num1 + " + " + num2 + "= " + output);
+        } else if (operator == "-") {
+            output = num1 - num2;
+            res.send("The result " + num1 + " - " + num2 + " =" + output);
+        } else if (operator == "/") {
+            output = num1 / num2;
+            res.send("The result " + num1 + " / " + num2 + "= " + output);
+        }
+        else if (operator == "%") {
+            output = num1 % num2;
+            res.send("The result " + num1 + " % " + num2 + "= " + output);
+        }
+        else if (operator == "*") {
+            output = num1 * num2;
+            res.send("The result " + num1 + " * " + num2 + " = " + output);
+        } else {
+            res.send(errorMessage);
+        }
+    }
+
+    calc(n1, n2, operator);
+
+});
+
+app.listen('3000', (res) => {
+    console.log("Connected to port 3000")
+})
